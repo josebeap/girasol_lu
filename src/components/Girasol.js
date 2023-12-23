c7ed3a39e4f8659ccfc18fa594a019c2cb09f5b6
josebeap <josebeap99@gmail.com>
2023-12-23 16:05:14 -0500

    Primer Commit Girasol

---
+++ src/components/Girasol.js
@@ -0,0 +1,28 @@
+import "./Girasol.css";
+
+function Girasol() {
+  return (
+    <body>
+      <div class="flower">
+        <div class="mid"></div>
+        <div class="Petal1 p1"></div>
+        <div class="Petal1 p2"></div>
+        <div class="Petal1 p3"></div>
+        <div class="Petal1 p4"></div>
+        <div class="Petal2 p1"></div>
+        <div class="Petal2 p2"></div>
+        <div class="Petal2 p3"></div>
+        <div class="Petal2 p4"></div>
+        <div class="Petal3 p1"></div>
+        <div class="Petal3 p2"></div>
+        <div class="Petal3 p3"></div>
+        <div class="Petal3 p4"></div>
+        <div class="stalk"></div> {/* Nuevo elemento para el tallo */}
+        <div class="leaf"></div> {/* Hoja */}
+        <div class="text-below">Con Cariño, para Lu 🌻</div>
+      </div>
+    </body>
+  );
+}
+
+export default Girasol;
